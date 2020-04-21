38355f16d9e43b40d5c1392074a91f37593c8f57
wxyice <920082206@qq.com>
2020-04-21 23:09:56 +0800

    add photos

--- tool.py
+++ tool.py
@@ -246,7 +246,7 @@ def git_operation():
 #     compress_photo()   # 压缩图片，并保存到mini_photos文件夹下
 #     git_operation()    # 提交到github仓库
 #     handle_photo()     # 将文件处理成json格式，存到博客仓库中
-#cut_photo()        # 裁剪图片，裁剪成正方形，去中间部分
+cut_photo()        # 裁剪图片，裁剪成正方形，去中间部分
 compress_photo()   # 压缩图片，并保存到mini_photos文件夹下
 git_operation()    # 提交到github仓库
 handle_photo()     # 将文件处理成json格式，存到博客仓库中
